04e783e31fbd32970c53c77c09801100662cfe00
Prince Vishwakarma <82967096+prince9318@users.noreply.github.com>
2023-05-02 16:03:42 +0530

    first commit

---
+++ src/fireabase.config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyDTrQDWyt00DtyU9UCTQKFcHmC1NM6af-g",
+  authDomain: "e-commerce-9ef55.firebaseapp.com",
+  projectId: "e-commerce-9ef55",
+  storageBucket: "e-commerce-9ef55.appspot.com",
+  messagingSenderId: "873634200090",
+  appId: "1:873634200090:web:33563cb02cb9ef7a59afa3"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+/* import { PersistGate } from "redux-persist/integration/react"; */
+import { PersistGate } from 'redux-persist/integration/react'
+import "react-toastify/dist/ReactToastify.css";
+import { store, persistor } from "./redux/store";
+import { app } from "./fireabase.config";
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider app={app} store={store}>
+    <PersistGate loading={"loading"} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
